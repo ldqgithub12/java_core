4fafd04b01610e355b2275e24cefc145441b32b5
ldqgithub12 <81614913+ldqgithub12@users.noreply.github.com>
2022-04-22 00:21:22 +0700

    fix chút xíu

--- Day-02/Homework/Homework/src/Bai1.java
+++ Day-02/Homework/Homework/src/Bai1.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Bai1 {
     public static void main(String[] args) {
-        float width, height;
+        float width, length;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the width: ");
         width = sc.nextFloat();
-        System.out.print("Enter the height: ");
-        height = sc.nextFloat();
+        System.out.print("Enter the length: ");
+        length = sc.nextFloat();
 
-        System.out.println("Perimeter(Chu vi) : "+((width+height)*2));
-        System.out.println("Area(Dien tich): "+(width*height));
+        System.out.println("Perimeter(Chu vi) : "+((width+length)*2));
+        System.out.println("Area(Dien tich): "+(width*length));
     }
 }
