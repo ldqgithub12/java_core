2f37e0406a5cc1dbb75da3c070fe284fe4fb33a5
ldqgithub12 <81614913+ldqgithub12@users.noreply.github.com>
2022-05-05 20:48:53 +0700

    upload 4b

--- Day_06/Kiemtra/Kiemtra/src/bai4.java
+++ Day_06/Kiemtra/Kiemtra/src/bai4.java
@@ -2,12 +2,20 @@ public class bai4 {
     public static void main(String[] args) {
         int i =0;
         int count = 0;
+        System.out.println("10 so nguyen to dau tien la: ");
         while (count <10){
             if(isPrimeNumber(i)){
                 System.out.print(i+" ");
                 count++;
             }
             i++;
+        }
+        System.out.println();
+        System.out.println("Cac so nguyen to nho hon 10 la");
+        for (int k =0; k<10;k++){
+            if (isPrimeNumber(k)){
+                System.out.print(k+" ");
+            }
         }
     }
 
